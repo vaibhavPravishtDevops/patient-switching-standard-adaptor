b2c73ba03ab3c1a5551ae0d3f83b4ccc70f03d40
vaibhavPravishtDevops <vaibhav.singh2@pravisht.com>
2026-03-26 14:25:54 +0530

    Initial commit - replicate logging bug

---
+++ gp2gp-translator/src/main/java/com/example/translator/TranslatorApplication.java
@@ -0,0 +1,26 @@
+package com.example.translator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class TranslatorApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(TranslatorApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslatorApplication.class, args);
+        logger.info("GP2GP Translator started successfully");
+    }
+
+    // Logs a message every 10 seconds so you can easily see if logs are being written
+    @Scheduled(fixedRate = 10000)
+    public void heartbeat() {
+        logger.info("[TRANSLATOR] Heartbeat - service is running. Working dir: {}", System.getProperty("user.dir"));
+    }
+}
---
+++ gpc-api-facade/src/main/java/com/example/facade/FacadeApplication.java
@@ -0,0 +1,26 @@
+package com.example.facade;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class FacadeApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(FacadeApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacadeApplication.class, args);
+        logger.info("GPC API Facade started successfully");
+    }
+
+    // Logs a message every 10 seconds so you can easily see if logs are being written
+    @Scheduled(fixedRate = 10000)
+    public void heartbeat() {
+        logger.info("[FACADE] Heartbeat - service is running. Working dir: {}", System.getProperty("user.dir"));
+    }
+}
